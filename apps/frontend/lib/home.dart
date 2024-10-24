45c11f0a3348aa0fae09e0f831651d6a5d6a9c38
Kwon Woojin <wojin57@gmail.com>
2024-10-24 17:46:36 +0900

    page files created

--- apps/frontend/lib/home.dart
+++ apps/frontend/lib/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:frontend/bulletin_board.dart';
+import 'package:frontend/square.dart';
+import 'package:frontend/profile.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,8 +12,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0; // initial selected tab index
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: <Widget>[],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_outlined),
+            label: '게시판',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline_rounded),
+            label: '광장',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box_outlined),
+            label: '프로필',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
   }
 }
