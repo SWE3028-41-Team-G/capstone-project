f8c1c53ffcf468664d31de0231b56f9e2de8fbee
ceun2 <toceun2@gmail.com>
2024-11-02 22:22:16 +0900

    feat(fe): modify and refactor Register

--- apps/frontend/lib/register.dart
+++ apps/frontend/lib/register.dart
@@ -11,7 +11,17 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final _formKey = GlobalKey<FormState>(); // GlobalKey for Form
 
-  String _idValue = "";
+  final _emailController = TextEditingController();
+  String? _selectedDomain;
+  String? selectedDoubleMajor;
+
+  void _onVerifyPressed() {
+    final email = '${_emailController.text}${_selectedDomain ?? ''}';
+    // 여기에 이메일 인증 로직 추가
+    print('인증할 이메일: $email');
+  }
+
+  //String _idValue = "";
   String _pwValue = "";
 
   @override
@@ -54,58 +64,31 @@ class _RegisterState extends State<Register> {
       body: SingleChildScrollView(
           child: Form(
         key: _formKey, // Assign the GlobalKey to Form
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: TextFormField(
-                // 이메일 입력 TextFormField --------------------------------
-                decoration: InputDecoration(
-                  labelText: "이메일을 입력해 주세요",
-                  floatingLabelBehavior:
-                      FloatingLabelBehavior.always, //labeltext 위에 고정
-                  contentPadding: EdgeInsets.all(20),
-                  border: OutlineInputBorder(),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 15, bottom: 5),
+                child: Text(
+                  "학교 구글 이메일 인증을 진행해 주세요",
                 ),
-                validator: (value) {
-                  // 입력값이 null 이거나 비었을 때 처리
-                  if (value == null || value.isEmpty) {
-                    return '이메일을 입력해 주세요.';
-                  }
-
-                  // 이메일 형식이 맞는지 확인
-                  if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                      .hasMatch(value)) {
-                    return '이메일 형식으로 입력해 주세요';
-                  }
-
-                  // 학교 이메일 형식 확인
-                  if (!(value.endsWith('@g.skku.edu') ||
-                      value.endsWith('@skku.edu'))) {
-                    return '학교 구글 이메일로 로그인해 주세요';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _idValue = value!;
-                },
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              child: TextFormField(
+              EmailInputRow(
+                emailController: _emailController,
+                selectedDomain: _selectedDomain,
+                onDomainChanged: (newDomain) {
+                  setState(() {
+                    _selectedDomain = newDomain;
+                  });
+                },
+                onVerifyPressed: _onVerifyPressed,
+              ),
+              CustomTextFormField(
+                labelText: "비밀번호를 입력해 주세요",
                 obscureText: true,
-                // 이메일 입력 TextFormField --------------------------------
-                decoration: InputDecoration(
-                  labelText: "비밀번호를 입력해 주세요",
-                  floatingLabelBehavior:
-                      FloatingLabelBehavior.always, //labeltext 위에 고정
-                  contentPadding: EdgeInsets.all(20),
-                  border: OutlineInputBorder(),
-                ),
                 validator: (value) {
-                  // 입력값이 null 이거나 비었을 때 처리
                   if (value == null || value.isEmpty) {
                     return '비밀번호를 입력해 주세요.';
                   }
@@ -115,104 +98,246 @@ class _RegisterState extends State<Register> {
                   _pwValue = value!;
                 },
               ),
-            ),
-            NumberRangeDropdown(),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              child: TextFormField(
-                // 이메일 입력 TextFormField --------------------------------
-                decoration: InputDecoration(
-                  labelText: "원전공을 입력해 주세요",
-                  floatingLabelBehavior:
-                      FloatingLabelBehavior.always, //labeltext 위에 고정
-                  contentPadding: EdgeInsets.all(20),
-                  border: OutlineInputBorder(),
-                ),
+              CustomTextFormField(
+                labelText: "비밀번호를 재입력해 주세요",
+                obscureText: true,
                 validator: (value) {
-                  // 입력값이 null 이거나 비었을 때 처리
+                  if (value == null || value.isEmpty) {
+                    return '비밀번호를 재입력해 주세요.';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _pwValue = value!;
+                },
+              ),
+              CustomTextFormField(
+                labelText: "원전공을 입력해 주세요",
+                validator: (value) {
                   if (value == null || value.isEmpty) {
                     return '원전공을 입력해 주세요.';
                   }
                   return null;
                 },
-                onSaved: (value) {
-                  // _pwValue = value!;
-                },
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              child: TextFormField(
-                // 이메일 입력 TextFormField --------------------------------
-                decoration: InputDecoration(
-                  labelText: "복수전공을 입력해 주세요",
-                  floatingLabelBehavior:
-                      FloatingLabelBehavior.always, //labeltext 위에 고정
-                  contentPadding: EdgeInsets.all(20),
-                  border: OutlineInputBorder(),
-                ),
+              Row(
+                children: [
+                  NumberRangeDropdown(),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    // width: MediaQuery.of(context).size.width / 2,
+                    // margin: EdgeInsets.symmetric(vertical: 10),
+                    child: DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        labelText: "복수전공 여부",
+                        border: OutlineInputBorder(),
+                      ),
+                      value: selectedDoubleMajor,
+                      items: [
+                        DropdownMenuItem(value: '해당없음', child: Text('해당없음')),
+                        DropdownMenuItem(value: '복수전공생', child: Text('복수전공생')),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          selectedDoubleMajor = value; // 선택된 값 업데이트
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null) {
+                          return '전공을 선택해 주세요.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              CustomTextFormField(
+                labelText: "복수전공을 입력해 주세요",
                 validator: (value) {
-                  // 입력값이 null 이거나 비었을 때 처리
                   if (value == null || value.isEmpty) {
                     return '복수전공을 입력해 주세요.';
                   }
                   return null;
                 },
-                onSaved: (value) {
-                  // _pwValue = value!;
-                },
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              child: TextFormField(
-                minLines: 5,
-                maxLines: 10,
-                // 원전공 정보 입력 TextFormField --------------------------------
-                decoration: InputDecoration(
-                  labelText: "원전공에 관한 정보를 입력해 주세요",
-                  floatingLabelBehavior:
-                      FloatingLabelBehavior.always, //labeltext 위에 고정
-                  contentPadding: EdgeInsets.all(20),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  // 입력값이 null 이거나 비었을 때 처리
-                  if (value == null || value.isEmpty) {
-                    return '원전공에 관한 정보를 입력해 주세요.';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  // _pwValue = value!;
-                },
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-              child: ElevatedButton(
-                //회원가입 버튼 -----------------------------------------
-                style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    backgroundColor: const Color.fromARGB(255, 30, 85, 33),
-                    side: BorderSide.none,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                  }
-                },
-                child: Text(
-                  "회원가입",
-                  style: TextStyle(color: Colors.white),
+              // CustomTextFormField(
+              //   labelText: "원전공에 관한 정보를 입력해 주세요",
+              //   minLines: 5,
+              //   maxLines: 10,
+              //   validator: (value) {
+              //     if (value == null || value.isEmpty) {
+              //       return '원전공에 관한 정보를 입력해 주세요.';
+              //     }
+              //     return null;
+              //   },
+              // ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: ElevatedButton(
+                  //회원가입 버튼 -----------------------------------------
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: const Color.fromARGB(255, 30, 85, 33),
+                      padding: EdgeInsets.symmetric(vertical: 13),
+                      side: BorderSide.none,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                    }
+                  },
+                  child: Text(
+                    "다음",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
+    );
+  }
+}
+
+// 이메일 입력 및 인증
+class EmailInputRow extends StatelessWidget {
+  final TextEditingController emailController;
+  final String? selectedDomain;
+  final Function(String?) onDomainChanged;
+  final VoidCallback onVerifyPressed;
+
+  const EmailInputRow({
+    super.key,
+    required this.emailController,
+    required this.selectedDomain,
+    required this.onDomainChanged,
+    required this.onVerifyPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12),
+      child: Row(
+        children: [
+          // 아이디 입력 필드
+          Expanded(
+            flex: 2,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "아이디",
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '아이디를 입력해 주세요.';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                emailController.text = value!;
+              },
+            ),
+          ),
+
+          SizedBox(width: 8),
+
+          // 도메인 선택 드롭다운
+          Expanded(
+            flex: 3,
+            child: DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: "도메인 선택",
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(),
+              ),
+              value: selectedDomain,
+              items: [
+                DropdownMenuItem(
+                    value: '@g.skku.edu', child: Text('@g.skku.edu')),
+                DropdownMenuItem(value: '@skku.edu', child: Text('@skku.edu')),
+              ],
+              onChanged: onDomainChanged,
+              validator: (value) {
+                if (value == null) {
+                  return '도메인을 선택해 주세요.';
+                }
+                return null;
+              },
+            ),
+          ),
+
+          SizedBox(width: 8),
+
+          // 인증 버튼
+          Expanded(
+            flex: 1,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  backgroundColor: const Color.fromARGB(255, 30, 85, 33),
+                  side: BorderSide.none,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: onVerifyPressed,
+              child: Text(
+                '인증',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// TextFormField 위젯 커스텀
+class CustomTextFormField extends StatelessWidget {
+  final String labelText;
+  final String? Function(String?)? validator;
+  final void Function(String?)? onSaved;
+  final bool obscureText;
+  final int? minLines;
+  final int? maxLines;
+
+  const CustomTextFormField({
+    super.key,
+    required this.labelText,
+    this.validator,
+    this.onSaved,
+    this.obscureText = false,
+    this.minLines,
+    this.maxLines,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 12),
+      child: TextFormField(
+        obscureText: obscureText,
+        minLines: minLines,
+        maxLines: maxLines ?? 1,
+        decoration: InputDecoration(
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          contentPadding: EdgeInsets.all(17),
+          border: OutlineInputBorder(),
+        ),
+        validator: validator,
+        onSaved: onSaved,
+      ),
     );
   }
 }
@@ -230,7 +355,7 @@ class _NumberRangeDropdownState extends State<NumberRangeDropdown> {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width / 2,
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      margin: EdgeInsets.symmetric(vertical: 10),
       child: DropdownButtonFormField<int>(
         decoration: InputDecoration(
           labelText: '학번을 선택해 주세요',
