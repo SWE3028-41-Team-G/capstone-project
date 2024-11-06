78f885a9395bc67f80ec3cccaeedad1734777db0
Kwon Woojin <wojin57@gmail.com>
2024-11-07 02:33:27 +0900

    message tab initial page implementation

--- apps/frontend/lib/message.dart
+++ apps/frontend/lib/message.dart
@@ -17,6 +17,94 @@ class MessageState extends State<Message> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Message',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        shape: Border(bottom: BorderSide(color: Colors.grey, width: 1)),
+      ),
+      body: Expanded(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(4, (index) {
+              String room_title =
+                  ["김성균", "이율전", "수학과-소프트웨어학과 복전생 모임", "농구 좋아하는 사람들 모임"][index];
+              String last_message = [
+                "감사합니다!",
+                "저도 그 수업 듣는데",
+                "과제 너무 많네요 ㅠㅠ",
+                "내일 저녁 8시 학교 코트에서 농구하실분?",
+              ][index];
+              int unread_message_counts = [2, 0, 6, 13][index];
+
+              return Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.grey, width: 2))),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Text(
+                            room_title,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  last_message,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(
+                                    '$unread_message_counts',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 10,
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
