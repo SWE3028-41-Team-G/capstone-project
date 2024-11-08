fd51c12fbffc2d4caa70985a1f830baad833d5eb
jspark2000 <idpjs2000@gmail.com>
2024-11-08 03:28:09 +0000

    fix: change ec2 .pem key

--- apps/aws/skkudm/ec2.tf
+++ apps/aws/skkudm/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "database" {
   ami                         = "ami-0c28dbbd4ed200038"
   instance_type               = "t4g.small"
   subnet_id                   = aws_subnet.private_1.id
-  key_name                    = "skkudm-key-pair"
+  key_name                    = "skkudm-keypair"
   vpc_security_group_ids      = [aws_security_group.ec2.id, data.aws_security_group.ssh-allow.id]
   associate_public_ip_address = true
 
