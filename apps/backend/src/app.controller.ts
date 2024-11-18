066ff24b88f0945a78556d09d99fcf2cf8985d77
jspark2000 <idpjs2000@gmail.com>
2024-11-18 07:42:09 +0000

    feat: add landing page renderer

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Render } from '@nestjs/common'
 import { AppService } from './app.service'
 import { Public } from './common/guard.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Public()
+  @Get()
+  @Render('index')
+  renderRendingPage() {
+    return {
+      title: 'SKKU-DM'
+    }
+  }
 
   @Public()
   @Get('/health-check')
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,17 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
 import cookieParser from 'cookie-parser'
+import { join } from 'path'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.use(cookieParser())
+
+  app.setBaseViewsDir(join(__dirname, 'views'))
+  app.setViewEngine('hbs')
 
   await app.listen(process.env.PORT ?? 3000)
 }
