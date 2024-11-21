2eae54bf16eb8bf36132daedd39497c876fdb3f2
Kwon Woojin <wojin57@gmail.com>
2024-11-21 17:30:03 +0900

    tag search API connection ready

--- apps/frontend/lib/bulletin_board/bulletin_board.dart
+++ apps/frontend/lib/bulletin_board/bulletin_board.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/bulletin_board/write_article.dart';
 
-// import 'package:http/http.dart' as http; // will be used later for API connection
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class BulletinBoard extends StatefulWidget {
   const BulletinBoard({super.key});
@@ -13,7 +14,44 @@ class BulletinBoard extends StatefulWidget {
 
 class BulletinBoardState extends State<BulletinBoard> {
   int _selectedFilterIndex = 0;
-  // List<dynamic> filters = [];
+  // Temporal infos, soon be linked with API
+  List<dynamic> filters = [
+    ["#수학", "#전공진입"],
+    ["#졸업요건", "#수업추천"],
+    ["#꿀팁", "#복수전공", "#수업추천"],
+    ["#수학", "#소프트웨어", "#CL과목", "#꿀팁"],
+  ];
+  List<dynamic> articles = [
+    {
+      "title": "이번 학기에 반드시 수강신청해야하는 과목",
+      "like": 12,
+      "comment": 3,
+      "content": "이번 학기에 새롭게 부임하신 이율전 교수님이 진행하시는 암호론 되시겠다. 우선 이 교수님...",
+      "tags": ["#수학", "#전공진입", "수업추천", "꿀팁"],
+    },
+    {
+      "title": "전공진입 수월하게 하는 꿀팁 공유",
+      "like": 6,
+      "comment": 1,
+      "content":
+          "다들 전공진입 어떻게 할 지 슬슬 고민되시죠? 전공진입 어느 학과까지 가능한지 쉽게 알 수 있는 꿀팁 공유해...",
+      "tags": ["#수학", "#전공진입"],
+    },
+    {
+      "title": "수강신청 망친 분들 참고하세요",
+      "like": 2,
+      "comment": 6,
+      "content": "참고 하라고 ㅋㅋㅋㅋㅋㅋㅋㅋ",
+      "tags": ["#수학", "#전공진입", "#도전학기"],
+    },
+    {
+      "title": "알고개 같이 공부하실 분 구해요~",
+      "like": 1,
+      "comment": 2,
+      "content": "월요일 9시 OOO 교수님 알고개 수업 같이  공부하실 분 구합니다!!!",
+      "tags": ["#수학", "#전공진입", "#소프트웨어", "#학점컷"],
+    },
+  ];
 
   @override
   void initState() {
@@ -39,6 +77,23 @@ class BulletinBoardState extends State<BulletinBoard> {
   //     print(response.statusCode);
   //   }
   // }
+
+  // API - GET search results
+  Future<void> _fetchArticles() async {
+    final response = await http.get(
+      Uri.parse('https://skku-dm.site/board?tag=tag'),
+      headers: {},
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        articles = jsonDecode(response.body)['articles'];
+      });
+    } else {
+      print('Debugging for GET - Searched articles list');
+      print(response.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -101,11 +156,10 @@ class BulletinBoardState extends State<BulletinBoard> {
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
-                    children: List.generate(4, (index) {
-                      String text = ['필터 #1', '필터 #2', '필터 #3', '필터 #4'][index];
+                    children: List.generate(filters.length, (index) {
                       return Row(
                         children: [
-                          if (index < 4) SizedBox(width: 15),
+                          if (index < filters.length) SizedBox(width: 15),
                           ElevatedButton(
                             onPressed: () {
                               setState(() {
@@ -119,7 +173,7 @@ class BulletinBoardState extends State<BulletinBoard> {
                             ),
                             child: Row(
                               mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[Text(text)],
+                              children: <Widget>[Text('필터#${index + 1}')],
                             ),
                           ),
                         ],
@@ -131,15 +185,16 @@ class BulletinBoardState extends State<BulletinBoard> {
                 Container(
                   margin: EdgeInsets.all(10),
                   child: Row(
-                    children: List.generate(4, (index) {
-                      String text = ['#수학', '#전공진입', '#수업추천', '#꿀팁'][index];
+                    children: List.generate(
+                        filters[_selectedFilterIndex].length, (index) {
                       return Row(
                         children: [
-                          if (index < 4) SizedBox(width: 15),
+                          if (index < filters[_selectedFilterIndex].length)
+                            SizedBox(width: 15),
                           Row(
                             mainAxisSize: MainAxisSize.min,
                             children: <Widget>[
-                              Text(text,
+                              Text(filters[_selectedFilterIndex][index],
                                   style: TextStyle(
                                       fontSize: 15, color: Colors.lightBlue))
                             ],
@@ -167,125 +222,133 @@ class BulletinBoardState extends State<BulletinBoard> {
           ),
           // results
           Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: List.generate(4, (index) {
-                  String title = [
-                    "이번 학기에 반드시 수강신청해야하는 과목",
-                    "전공진입 수월하게 하는 꿀팁 공유",
-                    "수강신청 망친 분들 참고하세요",
-                    "알고개 같이 공부하실 분 구해요~"
-                  ][index];
-                  int like = [12, 6, 2, 1][index];
-                  int comment = [3, 1, 6, 2][index];
-                  String content = [
-                    "이번 학기에 새롭게 부임하신 이율전 교수님이 진행하시는 암호론 되시겠다. 우선 이 교수님...",
-                    "다들 전공진입 어떻게 할 지 슬슬 고민되시죠? 전공진입 어느 학과까지 가능한지 쉽게 알 수 있는 꿀팁 공유해...",
-                    "참고 하라고 ㅋㅋㅋㅋㅋㅋㅋㅋ",
-                    "월요일 9시 수업 같이 듣고 공부하실 분 구합니다!!!",
-                  ][index];
-
-                  return Column(
-                    children: [
-                      if (index < 4) SizedBox(height: 15),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  margin: EdgeInsets.fromLTRB(15, 0, 10, 0),
-                                  child: Text(
-                                    title,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15,
+            child: ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(color: Colors.grey, width: 2))),
+                    child: Column(
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    margin: EdgeInsets.fromLTRB(15, 10, 10, 15),
+                                    child: Text(
+                                      "${articles[index]["title"]}",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
                                   ),
                                 ),
-                              ),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      CupertinoIcons.heart,
-                                      color: Colors.pinkAccent,
-                                      size: 16,
-                                    ),
-                                    Text('$like',
-                                        style: TextStyle(
-                                          color: Colors.pinkAccent,
-                                        )),
-                                  ],
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        CupertinoIcons.heart,
+                                        color: Colors.pinkAccent,
+                                        size: 16,
+                                      ),
+                                      Text("${articles[index]["like"]}",
+                                          style: TextStyle(
+                                            color: Colors.pinkAccent,
+                                          )),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              SizedBox(width: 3),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      Icons.comment,
-                                      color: Colors.black,
-                                      size: 16,
-                                    ),
-                                    Text('$comment',
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                        )),
-                                  ],
+                                SizedBox(width: 3),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.comment,
+                                        color: Colors.black,
+                                        size: 16,
+                                      ),
+                                      Text("${articles[index]["comment"]}",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                          )),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              SizedBox(width: 10),
-                            ],
-                          ),
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            margin: EdgeInsets.fromLTRB(20, 5, 10, 5),
-                            child: Text(
-                              content,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                              ),
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
+                                SizedBox(width: 10),
+                              ],
                             ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.all(10),
-                            child: Row(
-                              children: List.generate(4, (index) {
-                                String text =
-                                    ['#수학', '#전공진입', '#수업추천', '#꿀팁'][index];
-                                return Row(
-                                  children: [
-                                    if (index < 4) SizedBox(width: 15),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        Text(text,
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.lightBlue))
-                                      ],
-                                    ),
-                                  ],
-                                );
-                              }),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              margin: EdgeInsets.fromLTRB(20, 5, 10, 5),
+                              child: Text(
+                                "${articles[index]["content"]}",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
-                  );
-                }),
-              ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              child: Row(
+                                children: List.generate(
+                                    articles[index]["tags"].length, (idx) {
+                                  return Row(
+                                    children: [
+                                      if (index <
+                                          articles[index]["tags"].length)
+                                        SizedBox(width: 15),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          Text(articles[index]["tags"][idx],
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.lightBlue))
+                                        ],
+                                      ),
+                                    ],
+                                  );
+                                }),
+                              ),
+                            ),
+                            // children: List.generate(
+                            //     articles[index]["tags"].length, (index) {
+                            //   return Row(
+                            //     children: [
+                            //       if (index <
+                            //           articles[index]["tags"].length)
+                            //         SizedBox(width: 15),
+                            //       Row(
+                            //         mainAxisSize: MainAxisSize.min,
+                            //         children: <Widget>[
+                            //           Text(articles[index]["tags"],
+                            //               style: TextStyle(
+                            //                   fontSize: 15,
+                            //                   color: Colors.lightBlue))
+                            //         ],
+                            //       ),
+                            //     ],
+                            //   );
+                            // }),
+                            // ),
+                            // ),
+                          ],
+                        )
+                      ],
+                    ));
+              },
             ),
           ),
         ],
