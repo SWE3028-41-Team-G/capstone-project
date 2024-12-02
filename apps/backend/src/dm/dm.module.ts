e214b6d4bf2ee67b9e05530aaafb2894347d647f
jspark2000 <idpjs2000@gmail.com>
2024-12-02 12:36:38 +0000

    fix: dependency in dm module

--- apps/backend/src/dm/dm.module.ts
+++ apps/backend/src/dm/dm.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from '@/auth/auth.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule, type JwtModuleOptions } from '@nestjs/jwt'
 import { WsJwtGuard } from '@/auth/ws-jwt/ws.jwt.guard'
 import { DMGateway } from './dm.gateway'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        const options: JwtModuleOptions = {
+          secret: config.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '60m',
+            issuer: 'skku-dm.site'
+          }
+        }
+        return options
+      },
+      inject: [ConfigService]
+    })
+  ],
   providers: [DMGateway, WsJwtGuard]
 })
 export class DmModule {}
